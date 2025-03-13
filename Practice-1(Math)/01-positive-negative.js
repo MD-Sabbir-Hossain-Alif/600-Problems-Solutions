54e4ec853de4edc4053b9184dcb4a654b001fb96
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-03-13 21:56:46 +0600

    change something

--- Practice-1(Math)/01-positive-negative.js
+++ Practice-1(Math)/01-positive-negative.js
@@ -2,9 +2,9 @@ const numbers = [-5, 12, -8, 0, 3.5];
 let positive = [];
 let negative = [];
 
-for (let num of numbers) {
-    if (num > 0) positive.push(num);
-    if (num < 0) negative.push(num);
+for (let x of numbers) {
+    if (x > 0) positive.push(x);
+    if (x < 0) negative.push(x);
 }
 
 console.log(positive);
--- Practice-1(Math)/02-integer-even-odd.js
+++ Practice-1(Math)/02-integer-even-odd.js
@@ -2,21 +2,21 @@
 // Separation of Integer,Even,Odd
 
 const numbers = [23, 56, 19.69, 101, 42, 89, 11.37];
-let integer = [];
-let even = [];
-let odd = [];
+let integer = [], 
+    even = [],
+    odd = [];
 
 
-for (let num of numbers) {
+for (let x of numbers) {
     
-    if (Number .isInteger(num)) {
-        integer.push(num);
+    if (Number .isInteger(x)) {
+        integer.push(x);
 
-        if (num % 2 === 0) {
-            even.push(num);
+        if (x % 2 === 0) {
+            even.push(x);
         } 
         else {
-            odd.push(num);
+            odd.push(x);
         }
     }
 }
