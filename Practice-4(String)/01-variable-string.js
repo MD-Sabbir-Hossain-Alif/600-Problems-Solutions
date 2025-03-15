03b3912c02c6c169418fcbb31b8b9a6b19d128a1
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-03-15 23:28:37 +0600

    update folder name & add practice-4

---
+++ Practice-4(String)/01-variable-string.js
@@ -0,0 +1,47 @@
+
+//TODO: My name
+/* ------------------------------- Problem 01 ------------------------------- */
+const name = "Sabbir";
+console.log(name);
+// output: Sabbir
+
+
+//TODO: My address
+/* ------------------------------- Problem 02 ------------------------------- */
+const address = "Panchagarh";
+console.log(address);
+// output: Panchagarh
+
+
+//TODO: Best Friend university 
+/* ------------------------------- Problem 03 ------------------------------- */
+const bestFriend = "Jahangir Nagor University";
+console.log(address);
+// output: Jahangir Nagor University
+
+
+//TODO: Who is my heroine? !!!
+/* ------------------------------- Problem 04 ------------------------------- */
+let fan = "Muhammas (Sa.)";            //* i have no heroine but my hero is Muhammas (sa.)
+console.log(fan);
+// output: Muhammas (Sa.)
+
+
+//TODO: Movie of the year
+/* ------------------------------- Problem 05 ------------------------------- */
+const movieOfTheLife = "100% Halal"; //* i don"t watch movies now.
+console.log(movieOfTheLife);
+// output: 100% Halal
+//* by the way "100% Halal" this is a indonesian movie
+
+// TODO: what is your hobby
+/* ------------------------------- Problem 06 ------------------------------- */
+const hobby = "Traveling";
+console.log(hobby);
+// Output: Traveling
+
+// TODO: favorite color 
+/* ------------------------------- Problem 07 ------------------------------- */
+const color = "Blue";
+console.log(color);
+// Output: Blue
