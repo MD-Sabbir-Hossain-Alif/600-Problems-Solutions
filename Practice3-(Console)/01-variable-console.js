1962a33632c52a94c86e11e7e1e20f0963b1fec1
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-03-15 15:27:55 +0600

    vary intersting

---
+++ Practice3-(Console)/01-variable-console.js
@@ -0,0 +1,48 @@
+
+//TODO: My age
+/* ------------------------------- Problem 01 ------------------------------- */
+const age = 21;
+console.log(age);
+// output: 21
+
+
+//TODO: how many book you want to read
+/* ------------------------------- Problem 02 ------------------------------- */
+const booksToRead = 1;
+console.log(booksToRead);
+// output: 1
+
+
+//TODO: when you wake up 
+/* ------------------------------- Problem 03 ------------------------------- */
+let wakeUpTime = 5;
+    wakeUpTime = 6;
+console.log(wakeUpTime);
+// output: 6
+
+
+//TODO: how many time you waste in phone
+/* ------------------------------- Problem 04 ------------------------------- */
+let wastedTime = 0;            //* because i have no phone
+console.log(wastedTime);
+// output: 0
+
+
+//TODO: how much maney i borrow
+/* ------------------------------- Problem 05 ------------------------------- */
+const borrow = 3000;
+console.log(borrow, "Tk");
+// output: 3000 Tk
+
+
+// TODO: how many score your time
+/* ------------------------------- Problem 06 ------------------------------- */
+const goals = 4;
+console.log(goals);
+// Output: 4
+
+// TODO: how many pillow you have 
+/* ------------------------------- Problem 07 ------------------------------- */
+const pillow = 1;
+console.log(pillow);
+// Output: 1
