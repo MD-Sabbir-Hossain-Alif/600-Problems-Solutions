e7ae41b97442a71b28e4e0570ff3fcc1ea35cc7b
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-03-16 23:48:29 +0600

    variable name structure

---
+++ Practice-10(Keyword)/01-variable-structure.js
@@ -0,0 +1,59 @@
+
+//TODO: My favorite food name
+/* ------------------------------- Problem 01 ------------------------------- */
+const favoriteFood = "Meat";
+console.log(favoriteFood);
+// output: Meat
+
+
+//TODO: variable name case-sensitive or not
+/* ------------------------------- Problem 02 ------------------------------- */
+const city = "Chittagong";
+const City = "Dhaka";
+console.log(city);
+console.log(City);
+// output: Chittagong
+// output: Dhaka
+
+
+//TODO: chack variable name case-sensitive or not
+/* ------------------------------- Problem 03 ------------------------------- */
+let userName = "Johan";
+let username = "Jovan";
+console.log(userName);
+console.log(username);
+// output: Johan
+// output: Jovan
+
+
+//TODO: what is your home address
+/* ------------------------------- Problem 04 ------------------------------- */
+let myHomeAddress = "Panchagarh";
+console.log(myHomeAddress);
+// output: Panchagarh
+
+
+//TODO: how many runs in super over
+/* ------------------------------- Problem 05 ------------------------------- */
+
+//! "super" is a keyword can't use in variable
+
+let super = 20;
+console.log(super);
+// output: Uncaught SyntaxError: Unexpected token 'super'
+
+// TODO: what class do you read
+/* ------------------------------- Problem 06 ------------------------------- */
+
+//! "class" is a keyword can't use in variable
+let class = "Honours 2nd year";
+console.log(class);
+// Output: Uncaught SyntaxError: Unexpected token 'class'
+
+// TODO: variable declare
+/* ------------------------------- Problem 07 ------------------------------- */
+
+// TODO: we can not use number at beginning, instead we can use secoundPosition or Position2nd
+let 2ndPosition = "Tamim";
+console.log(2ndPosition);
+// Output: Uncaught SyntaxError: Invalid or unexpected token
