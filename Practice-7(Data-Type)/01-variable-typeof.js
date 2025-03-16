22fc1e9eb88720c0959b9e62947dbb1a3ff7bc89
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-03-16 16:03:32 +0600

    data type checking

---
+++ Practice-7(Data-Type)/01-variable-typeof.js
@@ -0,0 +1,42 @@
+
+//TODO: checking data type
+/* ------------------------------- Problem 01 ------------------------------- */
+let price = 555;
+console.log(typeof price);
+// output: number
+
+
+//TODO: checking data type
+/* ------------------------------- Problem 02 ------------------------------- */
+let learning = true;
+console.log(typeof learning);
+// output: boolean
+
+
+//TODO: checking data type
+/* ------------------------------- Problem 03 ------------------------------- */
+let megaCity = "Dhaka";
+console.log(typeof megaCity);
+// output: string
+
+
+//TODO: checking data type
+/* ------------------------------- Problem 04 ------------------------------- */
+let temperature = 102.5;
+
+console.log(typeof temperature);
+// output: number
+
+
+//TODO: checking data type
+/* ------------------------------- Problem 05 ------------------------------- */
+let isHungry = true; 
+console.log(typeof isHungry);
+// output: boolean
+
+
+// TODO: checking data type
+/* ------------------------------- Problem 06 ------------------------------- */
+const isRich = true;
+console.log(typeof isRich);
+// Output: boolean
