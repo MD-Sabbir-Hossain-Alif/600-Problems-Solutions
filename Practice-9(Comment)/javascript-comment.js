74ecafb169aa55ef50d7a7f5c1060159a1922f69
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-03-16 22:34:26 +0600

    commenting in code

---
+++ Practice-9(Comment)/javascript-comment.js
@@ -0,0 +1,60 @@
+
+//TODO: using "comment" for discribe variable
+/* ------------------------------- Problem 01 ------------------------------- */
+
+// hear variable score means runs in cricket
+let score = 20;
+
+//TODO: using "comment" for discribe variable
+/* ------------------------------- Problem 02 ------------------------------- */
+
+// it's a circle radius
+// We use it to find the circles area
+// using pi = 3.14
+let radius = 10;
+
+
+//TODO: using "comment" for discribe variable
+/* ------------------------------- Problem 03 ------------------------------- */
+
+/*
+black color pant 
+blue color panjabi
+maulticolor shirt 
+green color t-shirt
+*/
+let dress = 4;
+
+
+//TODO: using "comment" for discribe variable
+/* ------------------------------- Problem 04 ------------------------------- */
+
+// this money is gift from my aunt for buying chocolate 
+let giftMoney = 100;
+
+
+//TODO: using "comment" for discribe variable
+/* ------------------------------- Problem 05 ------------------------------- */
+
+/*
+Mathematics topic: understanding algebraic equations and their solutions
+Science topic: Newton's Laws of Motion and Their Applications
+History topic: The Rise and Fall of Ancient Civilizations
+Geography topic: Climate Change and Its Impact on Global Ecosystems
+*/
+let studyTime = 7;
+
+
+//TODO: using "comment" for discribe variable
+/* ------------------------------- Problem 06 ------------------------------- */
+
+// rosidul vai 500 tk paba
+// servic fee 10 tk
+let sendMoney = 200;
+
+
+//TODO: using "comment" for discribe variable
+/* ------------------------------- Problem 07 ------------------------------- */
+
+// in this ammount product price and dalivery fee is added 
+let pay = 1000;
