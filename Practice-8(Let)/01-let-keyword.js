22660f08f52a6dfe64ac4333b55a392a2404feee
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-03-16 16:27:57 +0600

    updateing value

---
+++ Practice-8(Let)/01-let-keyword.js
@@ -0,0 +1,53 @@
+
+
+//TODO: using let keyword declare variable and updateing value
+
+
+/* ------------------------------- Problem 01 ------------------------------- */
+let age = 20;
+    age = 21;
+console.log(age);
+// output: 21
+
+
+/* ------------------------------- Problem 02 ------------------------------- */
+let pocketMoney = 500;
+    pocketMoney = 150;
+console.log(pocketMoney);
+// output: 150
+
+
+/* ------------------------------- Problem 03 ------------------------------- */
+let studyTime = 4;
+    studyTime = 5;
+console.log(studyTime);
+// output: 5
+
+
+/* ------------------------------- Problem 04 ------------------------------- */
+let friendCount = 3;
+    friendCount = 1;
+console.log(friendCount);
+// output: 1
+
+
+/* ------------------------------- Problem 05 ------------------------------- */
+let phonePrice = 25000;
+    phonePrice = 12000;
+console.log(phonePrice);
+// output: 12000
+
+
+/* ------------------------------- Problem 06 ------------------------------- */
+let chairsCount = 40;
+    chairsCount = 38;
+    chairsCount = 32;
+console.log(chairsCount);
+// Output: 32
+
+
+/* ------------------------------- Problem 07 ------------------------------- */
+let playTime = 3;
+    playTime = 0;
+console.log(playTime);
+// Output: 0
