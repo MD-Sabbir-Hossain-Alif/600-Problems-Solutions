495e3ecb72547f537d2082e0bd8eb7250b77dbd9
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-03-16 13:13:13 +0600

    add practice-6

---
+++ Practice-6(Operator)/01-variable-math-operator.js
@@ -0,0 +1,103 @@
+
+//TODO: My income & expenditure
+/* ------------------------------- Problem 01 ------------------------------- */
+let income = 100,
+expenditure = 40;
+let remain = income -expenditure;
+console.log(remain);
+// output: 60
+
+
+//TODO: 10 pencils how many times i use(at a time 2)
+/* ------------------------------- Problem 02 ------------------------------- */
+let pencil = 10,
+useTime = 2;
+let useAtATime = pencil / useTime;
+console.log(useAtATime);
+// output: 5
+
+
+//TODO: how many times i can buy(at a time 15 Tk) & remain
+/* ------------------------------- Problem 03 ------------------------------- */
+let haveMoney = 60,
+    useMoney = 15;
+let useTimeMoney = haveMoney / useMoney;
+let remainMoney = haveMoney % useMoney;
+console.log(useTimeMoney);
+console.log(remainMoney);
+
+// output: 4
+// output: 0
+
+
+//TODO: print sting variable with operator
+/* ------------------------------- Problem 04 ------------------------------- */
+let string1 = "Hello ",
+    string2= "World";
+
+console.log(string1 + string2);
+// output: Hello World
+
+
+//TODO: total product
+/* ------------------------------- Problem 05 ------------------------------- */
+let rice = 153,
+    dal = 261;
+
+let sumProduct = rice + dal;
+console.log(sumProduct, "Kg");
+// output: 414 'Kg'
+
+
+// TODO: how many times i can buy(at a time 75 Tk) & remain
+/* ------------------------------- Problem 06 ------------------------------- */
+let haveMoney2 = 500,
+    useMoney2 = 75;
+let useTimeMoney2 = haveMoney2 / useMoney2;
+let remainMoney2 = haveMoney2 % useMoney2;
+console.log(useTimeMoney2);
+console.log(remainMoney2);
+// output: 6.666666666666667
+// output: 50
+
+
+// TODO: division and modulus
+/* ------------------------------- Problem 07 ------------------------------- */
+let x = 8,
+    y = 3;
+let division = x / y;
+let remainder = x % y;
+console.log(division);
+console.log(remainder);
+// output: 2.6666666666666665
+// output: 2
+
+
+// TODO: modulus
+/* ------------------------------- Problem 08 ------------------------------- */
+let m = 50,
+    n = 9;
+let modulus = m % n;
+console.log(modulus);
+// output: 5
+
+
+//TODO: print sting variable with operator
+/* ------------------------------- Problem 09 ------------------------------- */
+let string3 = "Bangla",
+    string4= "desh";
+
+console.log(string3 + string4);
+// output: Bangladesh
+
+
+// TODO: Monstar, 4 kg rice eat so, 480kg rice eat how many days?
+/* ------------------------------- Problem 10 ------------------------------- */
+let riceHave = 480,
+    iEat = 4;
+let howManyDays = riceHave / iEat;
+let howManyMonth = howManyMonth / 30;
+console.log(howManyDays, "Days");
+console.log(howManyMonth, "Month");
+// output: 120
+// output: 4
