db136c82d4b1d0ae2b14fed8e1d1b65680bca7da
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-04-04 22:38:59 +0600

    long time no update

---
+++ Chapter-04(Comparison)/Practice-012(Compare)/01-Number-Compare.js
@@ -0,0 +1,72 @@
+
+//TODO: comparsion between two brother
+/* ------------------------------- Problem 01 ------------------------------- */
+let iEat = 7,
+    smallBroEat = 5;
+
+let compare = iEat > smallBroEat;
+console.log(compare);
+// output: true
+
+
+//TODO: comparsion between class student
+/* ------------------------------- Problem 02 ------------------------------- */
+let myClass = 45,
+    nextClass = 50;
+
+let moreStudent = nextClass > myClass;
+console.log(moreStudent);
+// output: true
+
+
+//TODO: comparsion between result
+/* ------------------------------- Problem 03 ------------------------------- */
+let myMarks = 75,
+    friendMarks = 75;
+
+let mostMarks = myMarks == friendMarks;
+console.log(mostMarks);
+// output: true
+
+
+
+//TODO: targeting to saveing money
+/* ------------------------------- Problem 04 ------------------------------- */
+let targetSaveMoney = 100,
+    saveMoney = 95;
+
+let targetFillUp = targetSaveMoney <= saveMoney;
+console.log(targetFillUp);
+// output: false
+
+
+//TODO: naumber comparsion
+/* ------------------------------- Problem 05 ------------------------------- */
+let num1 = 10,
+    num2 = 7;
+
+let different = num1 != num2;
+console.log(different);
+// output: true
+
+
+//TODO: naumber comparsion
+/* ------------------------------- Problem 06 ------------------------------- */
+
+let numX = 20,
+    numY = 15;
+
+let Compare = numX <= numY;
+console.log(Compare);
+// output: false
+
+
+//TODO: comparsion between result
+/* ------------------------------- Problem 06 ------------------------------- */
+
+let sabbirsMarks = 45,
+    friendsMarks = 97;
+
+let lowest = friendsMarks <= sabbirsMarks;
+console.log(lowest);
+// output: false
