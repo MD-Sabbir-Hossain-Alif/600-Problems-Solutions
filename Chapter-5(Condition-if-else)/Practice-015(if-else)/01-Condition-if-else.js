0d773646338e7ccbe020c4951ba57b4424596bb6
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-04-08 22:46:51 +0600

    oh if else condition is so funny

---
+++ Chapter-5(Condition-if-else)/Practice-015(if-else)/01-Condition-if-else.js
@@ -0,0 +1,76 @@
+
+//TODO: mother's breakfast condition
+/* ------------------------------- Problem 01 ------------------------------- */
+const breakfastTime = 6;
+let currentTime = 5;
+
+if (currentTime < backTime) {
+    console.log("give breakfast");
+} else {
+    console.log("give broomstick punisment")
+}
+// output: give breakfast
+
+
+//TODO: app login display
+/* ------------------------------- Problem 02 ------------------------------- */
+const password = 60405020;
+let inputPassword = 30405060;
+
+if (password === inputPassword) {
+    console.log("Welcome!");
+} else {
+    console.log("Get Lost!")
+}
+// output: Get Lost!
+
+
+//TODO: running condition
+/* ------------------------------- Problem 03 ------------------------------- */
+const minimumRun = 5;
+let iRun = 7;
+
+if (minimumRun <= iRun) {
+    console.log("You got Chocolate!");
+} else {
+    console.log("Fat Balli!")
+}
+// output: You got Chocolate!
+
+
+//TODO: Father's Bike condition
+/* ------------------------------- Problem 04 ------------------------------- */
+const targetMarks = 80,
+    myMarks = 85;
+
+if (targetMarks < myMarks) {
+    console.log("Abbu will give Bike");
+} else {
+    console.log("Abbu will not give Bike")
+}
+// output: Abbu will give Bike
+
+
+
+//TODO: condition for watching movie
+/* ------------------------------- Problem 05 ------------------------------- */
+const movieShowTime = 10;
+
+if (9 > movieShowTime) {
+    console.log("I will watch movie");
+} else {
+    console.log("I will sleep with oil in the nose")
+}
+// output: I will sleep with oil in the nose
+
+
+//TODO: condition for tunning on AC
+/* ------------------------------- Problem 06 ------------------------------- */
+const temperature = 30;
+
+if (temperature >= 28) {
+    console.log("AC ON");
+} else {
+    console.log("I will sleep with blanket")
+}
+// output: I will sleep with blanket
