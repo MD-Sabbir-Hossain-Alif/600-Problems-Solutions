ba267723b45c855bad30fba0eb18c55254a2ebc3
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-04-09 16:12:39 +0600

    if else with and-or conditions

---
+++ Chapter-5(Condition-if-else)/Practice-016(AND-OR)/01-Condition-AND-OR.js
@@ -0,0 +1,117 @@
+
+//TODO: vehicle condition
+/* ------------------------------- Problem 01 ------------------------------- */
+const age = 20;
+const height = 70;
+
+if (age > 18 && height > 60) {
+    console.log("will push vehicle");
+} else {
+    console.log("sit in vehicle")
+}
+// output: will push vehicle
+
+
+//TODO: admission condition
+/* ------------------------------- Problem 02 ------------------------------- */
+const mathMarks = 90;
+const englishMarks = 80;
+
+if (mathMarks > 80 && englishMarks > 90) {
+    console.log("You are eligible for admission");
+} else {
+    console.log("I will marry you off!")
+}
+// output: I will marry you off!
+
+
+//TODO: scholarship condition
+/* ------------------------------- Problem 03 ------------------------------- */
+const GPA = 5;
+const monthlyIncome = 9000;
+
+if (GPA == 5 && monthlyIncome < 10000) {
+    console.log("You are eligible for scholarship");
+} else {
+    console.log("You are not eligible for scholarship")
+}
+// output: You are eligible for scholarship
+
+
+//TODO: job exam participate condition
+/* ------------------------------- Problem 04 ------------------------------- */
+const personAge = 35;
+const experience = 3;
+
+if (personAge < 30 && experience > 2) {
+    console.log("You are eligible for job exam");
+} else {
+    console.log("You are not eligible for job exam")
+}
+// output: You are not eligible for job exam
+
+
+
+//TODO: condition for cocking egg korma
+/* ------------------------------- Problem 05 ------------------------------- */
+const eggAmount = 15;
+const hen = true;
+
+if (eggAmount > 12 || hen == false) {
+    console.log("I will cock egg korma");
+} else {
+    console.log("I will eat bread and banana")
+}
+// output: I will cock egg korma
+
+
+//TODO: condition for doctors advice
+/* ------------------------------- Problem 06 ------------------------------- */
+const temperature = 102;
+const cough = true;
+
+if (temperature > 100 || cough == true) {
+    console.log("I will go for the doctors advice");
+} else {
+    console.log("I will not go for the doctors advice")
+}
+// output: I will go for the doctors advice
+
+
+//TODO: condition for attend test
+/* ------------------------------- Problem 07 ------------------------------- */
+const attendence = 79;
+const  homework = true;
+
+if (attendence > 80 && homework == true) {
+    console.log("You can take the test");
+} else {
+    console.log("Auto Fail!")
+}
+// output: Auto Fail!
+
+
+//TODO: condition for read or playing video games
+/* ------------------------------- Problem 08 ------------------------------- */
+const eletricity = false;
+const mobileCharge = false;
+
+if (eletricity == false && mobileCharge == false) {
+    console.log("I will sit down to read");
+} else {
+    console.log("I will play video games")
+}
+// output: I will sit down to read
+
+
+//TODO: condition for doctors advice
+/* ------------------------------- Problem 09 ------------------------------- */
+const shirtPrice = 1099;
+const coupon= true;
+
+if (shirtPrice > 1000 && coupon == true) {
+    console.log("You will get discount!!");
+} else {
+    console.log("You are not eligible for discount")
+}
+// output: You will get discount!!
