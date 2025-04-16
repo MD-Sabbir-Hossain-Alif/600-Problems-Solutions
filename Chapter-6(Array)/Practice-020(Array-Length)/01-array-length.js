9020ea318e9e5925b712248f0504a714ca955cb9
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-04-16 15:05:40 +0600

    array length

---
+++ Chapter-6(Array)/Practice-020(Array-Length)/01-array-length.js
@@ -0,0 +1,47 @@
+
+//TODO: 71 - 79 numbers array
+/* ------------------------------- Problem 01 ------------------------------- */
+const numbers = [71, 72, 73, 74, 75, 76, 77, 78,79];
+
+
+//TODO: fruits array
+/* ------------------------------- Problem 02 ------------------------------- */
+const fruits  = ["Apple", "Banana", "Orange", "Mango", "Guava"];
+
+
+//TODO: vegetable array length
+/* ------------------------------- Problem 03 ------------------------------- */
+const vegetable = ["Potato", "Onion", "Brinjal", "Radish", "Carrot", "Tomato", "Orka", "Lauki", "Cucumber", "Parwal"]
+const howMany = vegetable.length;
+
+console.log(howMany);
+
+// Output: 10
+
+//TODO: Movie name in array
+/* ------------------------------- Problem 04 ------------------------------- */
+const favMovie = ["3Idiots", "Dipu Number 2", "Inception", "The Matrix", "The Dark Knight"];
+
+
+//TODO: 11 - 30 odd numbers
+/* ------------------------------- Problem 05 ------------------------------- */
+const odd = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29];
+
+
+//TODO: condition for logical not operator
+/* ------------------------------- Problem 06 ------------------------------- */
+const colors = ["Red", "Green", "Blue", "Yellow", "Orange", "Indigo", "Violet"]
+const howManyColors = colors.length;
+
+console.log(howManyColors);
+
+// Output: 7
+
+//TODO: condition for logical not operator
+/* ------------------------------- Problem 07 ------------------------------- */
+const countries = ["Dhaka", "Ramallah", "Singapore", "Doha", "Kabul", "Jakarta",]
+const howManyCountries = countries.length;
+
+console.log(howManyCountries);
+
+// Output: 6
