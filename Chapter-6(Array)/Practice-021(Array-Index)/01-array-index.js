f08e87e5c7e7fcf26f53ca91cd28a19ce6faf7a0
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-04-18 21:47:53 +0600

    index in array

---
+++ Chapter-6(Array)/Practice-021(Array-Index)/01-array-index.js
@@ -0,0 +1,55 @@
+
+//TODO: index 3 friend in array
+/* ------------------------------- Problem 01 ------------------------------- */
+const friends = ["Anik", "Emon", "Shohag", "Sabbir Khan", "Ridoy", "Tonmmoy", "Turjo", "Sovhon", "Ruman", "Najmul"]
+
+console.log(friends[3]);
+
+// Output: Sabbir
+
+
+//TODO: index 5 fav books in array
+/* ------------------------------- Problem 02 ------------------------------- */
+const favBooks = ["Quran", "Riadus Solehin", "Paradoxical Sajid-1", "Paradoxical Sajid-2", "Bela Furaber Aga", "Raha Balayet", "Logics"]
+
+console.log(favBooks[5]);
+
+// Output: Raha Balayet
+
+
+//TODO: 7 number position update in num array
+/* ------------------------------- Problem 03 ------------------------------- */
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+num[6] = 30;  // position 7 = index 6
+
+
+//TODO: mobile games array update
+/* ------------------------------- Problem 04 ------------------------------- */
+const games = ["PUBG", "Temple Run", "Free Fire", "Candy Crash"];
+games[2] = "Subway Saffer";
+
+
+//TODO: 11 - 30 odd numbers
+/* ------------------------------- Problem 05 ------------------------------- */
+const famliysName = ["Ibrahim", "aysa", "Sabbir", "Tabasum"];
+
+console.log(famliysName[2]);  // position 3 = index 2
+
+
+//TODO: traval country position array
+/* ------------------------------- Problem 06 ------------------------------- */
+const countries = ["Bangladesh", "Afghanistan", "Singapore", "Palestine", "Katar", "Indonashia"]
+
+
+console.log(countries[3]); // position 4 = index 3
+
+// Output: Palestine
+
+//TODO: 7th position in items array
+/* ------------------------------- Problem 07 ------------------------------- */
+const tebleItems = ["Lamp", "Clock", "Books", "Laptop"]
+
+
+console.log(tebleItems[7]);
+
+// Output: undefined
