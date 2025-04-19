ecf35f2a40acd055d445282119b5754f18771627
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-04-19 21:05:09 +0600

    push-pop-shift-unshift

---
+++ Chapter-6(Array)/Practice-022(Push-POP)/01-array-push-pop.js
@@ -0,0 +1,47 @@
+
+//TODO: posh 60 in numbers array
+/* ------------------------------- Problem 01 ------------------------------- */
+const numbers = [10, 20, 30, 40, 50];
+numbers.push(60);
+
+console.log(numbers);
+
+// Output: [10, 20, 30, 40, 50, 60]
+
+
+//TODO: push friend name in friends array
+/* ------------------------------- Problem 02 ------------------------------- */
+const friends = ["Sajib", "Sagor", "Sakib", "Sohal",]
+friends.push("Sumon");
+
+console.log(friends);
+
+// Output: ['Sajib', 'Sagor', 'Sakib', 'Sohal', 'Sumon']
+
+
+//TODO: pop out last mobile game from array
+/* ------------------------------- Problem 03 ------------------------------- */
+const games = ["PUBG", "Temple Run", "Free Fire", "Candy Crash"];
+games.pop();
+
+console.log(games);
+
+// Output: ['PUBG', 'Temple Run', 'Free Fire']
+
+//TODO: number 12 unshift (in front) number array
+/* ------------------------------- Problem 04 ------------------------------- */
+const number = [24, 36, 48, 60];
+number.unshift(12);
+
+console.log(number);
+
+// Output: [12, 24, 36, 48, 60]
+
+//TODO: remove first read book froms books array
+/* ------------------------------- Problem 05 ------------------------------- */
+const books = ["Computer Basics", "Raha Balaet", "Marhaba javascrip e maro thaba", "Islami akida"]
+books.shift();
+
+console.log(books);
+
+// Output: ['Raha Balaet', 'Marhaba javascrip e maro thaba', 'Islami akida']
