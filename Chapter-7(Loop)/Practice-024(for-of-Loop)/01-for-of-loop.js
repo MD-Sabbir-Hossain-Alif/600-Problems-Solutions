d967af67d7a6124d409c2ce86524d689dfba3d6d
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-04-20 22:18:16 +0600

    for of loop ah nice

---
+++ Chapter-7(Loop)/Practice-024(for-of-Loop)/01-for-of-loop.js
@@ -0,0 +1,103 @@
+
+//TODO: favorite books print using for of loop
+/* ------------------------------- Problem 01 ------------------------------- */
+const favoriteBooks = ["Computer Basics", "Computer Programming", "Math", "Physics", "Chemistry"];
+
+for (const bookItems of favoriteBooks) {
+    console.log(bookItems);
+}
+
+/* Output:
+
+Computer Basics
+Computer Programming
+Math
+Physics
+Chemistry
+
+*/
+
+
+//TODO: favorite foods print using for of loop
+/* ------------------------------- Problem 02 ------------------------------- */
+const favFoods = ["Mishti ", "Shorshe Ilish", "Beef", "Chicken Roast", "Kacchi Biryani"];
+
+for (const foods of favFoods) {
+    console.log(foods);
+}
+
+/* Output:
+
+Mishti
+Shorshe Ilish
+Beef
+Chicken Roast
+Kacchi Biryani
+
+*/
+
+
+//TODO: family Mamber birth Year print using for of loop
+/* ------------------------------- Problem 03 ------------------------------- */
+const birthYear = [1975, 1985, 2002, 2009];
+
+for (const year of birthYear) {
+    console.log(year);
+}
+
+/* Output:
+
+1975
+1985
+2000
+2008
+
+*/
+
+
+//TODO: football player my favorite 11 display using for of loop
+/* ------------------------------- Problem 04 ------------------------------- */
+const favFootballPlayers = ["Mohamed Salah", "Sadio Mane", "Kalidou Koulibaly", "N'Golo Kante",
+    "Riyad Mahrez", "Karim Benzema", "Mesut Ozil", "Hamza Choudhury", "Edouard Mendy", "Achraf Hakimi", "Kalidou Koulibaly"];
+
+for (const player of favFootballPlayers) {
+    console.log(player)
+}
+
+
+/* Output:
+
+Mohamed Salah
+Sadio Mane
+Kalidou Koulibaly
+N'Golo Kante
+Riyad Mahrez
+Karim Benzema
+Mesut Ozil
+Hamza Choudhury
+Edouard Mendy
+Achraf Hakimi
+Kalidou Koulibaly
+
+*/
+
+
+//TODO: next Exam date display using for of loop
+/* ------------------------------- Problem 05 ------------------------------- */
+const examDates = ["21-4-2025", "24-4-2025", "28-4-2025", "1-5-2025", "5-5-2025", "8-5-2025", "12-5-2025"];
+
+for (const dates of examDates) {
+    console.log(dates);
+}
+
+/* Output:
+
+21-4-2025
+24-4-2025
+28-4-2025
+1-5-2025
+5-5-2025
+8-5-2025
+12-5-2025
+
+*/
