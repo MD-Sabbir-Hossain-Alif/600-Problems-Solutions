60614b9d61be4cb39b4c6f9804b405daf9b64ca8
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-04-22 23:41:41 +0600

    while loop is mind blowing

---
+++ Chapter-7(Loop)/Practice-025(while-Loop)/01-while-loop.js
@@ -0,0 +1,160 @@
+
+//TODO: printing 10 times using while loop
+/* ------------------------------- Problem 01 ------------------------------- */
+let num = 0 ;
+while (num < 10) {
+    console.log("I will practice programing for 6 hours");
+    num++;
+}
+
+// Output: (10)I will practice programing 6 hour'sjkks
+
+
+//TODO: printing 1 to 20 using while loop
+/* ------------------------------- Problem 02 ------------------------------- */
+let number = 1;
+while (number <= 20) {
+    console.log(number);
+    number++;
+}
+
+/* Output:
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+
+*/
+
+
+//TODO: printing 50 to 100 using while loop
+/* ------------------------------- Problem 03 ------------------------------- */
+let numbers = 50;
+while (numbers <= 100) {
+    console.log(numbers);
+    numbers++;
+}
+
+/* Output:
+
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
+
+*/
+
+
+//TODO: printing 1 to 10 and multiply with 3 using while loop
+/* ------------------------------- Problem 04 ------------------------------- */
+let numBER = 1;
+while (numBER <= 10) {
+    const result = 3 * numBER;
+    console.log(result);
+    numBER++;
+
+}
+
+/* Output:
+
+3
+6
+9
+12
+15
+18
+21
+24
+27
+30
+
+*/
+
+
+//TODO: printing 1 to 110 and multiply with 3 using while loop
+/* ------------------------------- Problem 05 ------------------------------- */
+let num2 = 111;
+while (num2 <= 120) {         // in this problem page number 109 Practice 25; no.5
+    const result = num2 / 2;  //111 to 110 it's a type error!! so i type 120
+    console.log(result);
+    num2++;
+}
+
+/* Output:
+
+55.5
+56
+56.5
+57
+57.5
+58
+58.5
+59
+59.5
+60
+
+*/
