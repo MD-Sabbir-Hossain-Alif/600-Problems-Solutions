3ebfc93476bdcbd8387d1bd7b1fde2bdbbda9b72
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-04-23 22:59:32 +0600

    summation number using while loop

---
+++ Chapter-7(Loop)/Practice-026(while-addition)/02-while-addition.js
@@ -0,0 +1,12 @@
+
+//TODO: printing 5 to 15 summation using while loop
+/* ------------------------------- Problem 02 ------------------------------- */
+let number = 5;
+let sum = 0;
+while (number <= 15) {
+    sum = sum + number;
+    number++;
+}
+console.log(sum);
+
+// Output: 110
---
+++ Chapter-7(Loop)/Practice-026(while-addition)/05-while-addition.js
@@ -0,0 +1,11 @@
+//TODO: printing 1 to 110 and multiply with 3 using while loop
+/* ------------------------------- Problem 05 ------------------------------- */
+let num = 20;
+let sum = 0;
+while (num <= 40) {
+    sum = sum + num;
+    num++;
+}
+console.log('sum:', sum);
+
+//  Output: sum: 630
