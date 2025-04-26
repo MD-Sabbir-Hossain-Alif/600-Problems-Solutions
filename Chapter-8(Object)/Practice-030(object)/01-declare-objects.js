1f4de20347d598ff46276abba230d03895dd5b9c
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-04-26 09:46:40 +0600

    object declare

---
+++ Chapter-8(Object)/Practice-030(object)/01-declare-objects.js
@@ -0,0 +1,82 @@
+
+//TODO: create an object named teacher with 5 properties
+/* ------------------------------- Problem 01 ------------------------------- */
+
+const teacher = {
+    name: 'Arafat Hossain',
+    subject: 'Logic',
+    age: 32,
+    home: 'Rangpur',
+    title: 'Lecturer'
+}
+
+
+//TODO: create an object named tree with some properties
+/* ------------------------------- Problem 02 ------------------------------- */
+
+const tree = {
+    name: 'Coconut Tree',
+    height: 20,
+    age: 25,
+    fruitName: 'Coconut',
+    shape: 'Round'
+}
+
+
+//TODO: create an object named animals with some properties
+/* ------------------------------- Problem 03 ------------------------------- */
+
+const animals = {
+    name: 'Cat',
+    color: 'White & Gray',
+    voice: 'Mew',
+    lifeTime: 20,
+    favFood: 'Fish',
+}
+
+
+//TODO: create an object named father with some properties
+/* ------------------------------- Problem 04 ------------------------------- */
+
+const father = {
+    name: 'Monirul',
+    profession: "Teacher",
+    age: 50,
+    children: 2,
+    favFood: "Meat"
+}
+
+
+//TODO: create an object named motorbike with some properties
+/* ------------------------------- Problem 05 ------------------------------- */
+
+const motorbike = {
+    brand: "GPX",
+    model: "Demon RR",
+    wheels: 2,
+    color: "Yellow & Gray",
+    maxspeed: 130,
+    price: 371000
+}
+
+
+//TODO: create an object named bird with some properties
+/* ------------------------------- Problem 06 ------------------------------- */
+
+const bird = {
+    name: "Eagle",
+    color: "Dark Brown",
+    food:"fish & snakes etc",
+    weight: 30
+}
+
+//TODO: create an object named laptop with some properties
+/* ------------------------------- Problem 07 ------------------------------- */
+
+const laptop = {
+    barnd: "HP",
+    Processor: "AMD",
+    ram: "8 GB",
+    price: 49500,
+    display: 14
+}
