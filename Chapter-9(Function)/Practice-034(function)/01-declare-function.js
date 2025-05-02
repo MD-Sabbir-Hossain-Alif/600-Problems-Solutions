546832e7d042f9aded450af43c20e2ea8e169c3b
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-05-02 10:10:52 +0600

    function i love this

---
+++ Chapter-9(Function)/Practice-034(function)/01-declare-function.js
@@ -0,0 +1,121 @@
+
+//TODO: declare a function named jusKhao with some code
+/* ------------------------------- Problem 01 ------------------------------- */
+
+function jusKhao() {
+    console.log("Ai Nao Labur Jus");
+};
+jusKhao();
+
+// Output: Ai Nao Labur Jus
+
+
+//TODO: declare a function named myRoutine with block of code
+/* ------------------------------- Problem 02 ------------------------------- */
+
+function myRoutine() {
+    console.log("I wake up early in the morning ");
+    console.log("I atented Fajar Prayer");
+    console.log("After prayer i eat some brackfast");
+    console.log("Also i eat tea");
+    console.log("Finaly i set in char and practice code");
+    console.log("6 hour code")
+};
+myRoutine();
+
+/* Output: 
+I wake up early in the morning 
+I atented Fajar Prayer
+After prayer i eat some brackfast
+Also i eat tea
+Finaly i set in char and practice code
+6 hour code
+*/
+
+
+//TODO: declare a function named familyMamber with block of code
+/* ------------------------------- Problem 03 ------------------------------- */
+
+function familyMamber() {
+    console.log("First Mamber: Monirul");
+    console.log("Second Mamber: Shaida");
+    console.log("Third Mamber: Sabbir");
+    console.log("Fourth Mamber: Tabasum");
+};
+familyMamber();
+
+/* Output: 
+First Mamber: Monirul
+Second Mamber: Shaida
+Third Mamber: Sabbir
+Fourth Mamber: Tabasum
+*/
+
+
+//TODO: declare a function named taskDone with block of code
+/* ------------------------------- Problem 04 ------------------------------- */
+
+function taskDone() {
+    console.log("Deep Work Coding Session (3–4 hrs)");
+    console.log("Learning/Skill Sharpening (1 hr)");
+    console.log("Code Review + Communication (1 hr)");
+    console.log("Physical Exercise / Walk (30–60 mins)");
+    console.log("Reflection + Planning (15–30 mins)")
+};
+taskDone();
+
+/* Output: 
+Deep Work Coding Session (3–4 hrs)
+Learning/Skill Sharpening (1 hr)
+Code Review + Communication (1 hr)
+Physical Exercise / Walk (30–60 mins)
+Reflection + Planning (15–30 mins)
+*/
+
+
+//TODO: declare a function named greatMe with some code
+/* ------------------------------- Problem 05 ------------------------------- */
+
+function greatMe() {
+    console.log("Good Morning! Have a great Day!");
+};
+greatMe();
+
+// Output: Good Morning! Have a great Day!
+
+
+//TODO: declare a function named getFavFriends with block of code
+/* ------------------------------- Problem 06 ------------------------------- */
+
+function getFavFriends() {
+    console.log("Friend 1: Emon");
+    console.log("Friend 2: Shohag");
+    console.log("Friend 3: Najmul");
+};
+getFavFriends();
+
+/* Output: 
+Friend 1: Emon
+Friend 2: Shohag
+Friend 3: Najmul
+*/
+
+//TODO: declare a function named with block of code
+/* ------------------------------- Problem 07 ------------------------------- */
+
+function introduceMyself() {
+    console.log("name: Sabbir");
+    console.log("age: 22");
+    console.log("address: Rangpur");
+    console.log("phone: 01300.......");
+    console.log("height: 5fit 6inch");
+};
+introduceMyself();
+
+/* Output: 
+name: Sabbir
+age: 22
+address: Rangpur
+phone: 01300.......
+height: 5fit 6inch
+*/
