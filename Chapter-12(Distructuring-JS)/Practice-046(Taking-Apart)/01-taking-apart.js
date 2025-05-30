7ccafeb9ccb115aefc0f909d4da8201d5fa69e5e
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-05-30 10:30:44 +0600

    feat: taking apart using destructuring syntax

---
+++ Chapter-12(Distructuring-JS)/Practice-046(Taking-Apart)/01-taking-apart.js
@@ -0,0 +1,69 @@
+
+//TODO: taking apart from objcet
+/* ------------------------------- Problem 01 ------------------------------- */
+
+const product = {
+    name: "Laptop",
+    price: 32500,
+    brand: "HP"
+};
+const x = product.brand;    // we can asign a new variable but 
+const {brand} = product;    // destructuring obj value using this syntax;
+
+console.log(brand);
+console.log(x);
+
+
+// Output: HP
+
+
+//TODO: 
+/* ------------------------------- Problem 02 ------------------------------- */
+
+
+// Output: 
+
+
+//TODO: 
+/* ------------------------------- Problem 03 ------------------------------- */
+
+
+
+// Output: 
+
+
+//TODO: 
+/* ------------------------------- Problem 04 ------------------------------- */
+
+
+
+// Output: 
+
+
+//TODO: 
+/* ------------------------------- Problem 05 ------------------------------- */
+
+
+
+// Output: 
+
+//TODO: 
+/* ------------------------------- Problem 06 ------------------------------- */
+
+
+
+// Output: 
+
+//TODO: 
+/* ------------------------------- Problem 07 ------------------------------- */
+
+
+
+// Output: 
+
+//TODO: 
+/* ------------------------------- Problem 08 ------------------------------- */
+
+
+
+// Output: 
