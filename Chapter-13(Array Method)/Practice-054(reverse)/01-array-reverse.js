d81900e90d69eea61f28cc7c159da26dd8316a6a
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-06-15 07:45:47 +0600

    feat: revarse method

---
+++ Chapter-13(Array Method)/Practice-054(reverse)/01-array-reverse.js
@@ -0,0 +1,36 @@
+
+//TODO: Reverse the order of names in the array
+/* ------------------------------- Problem 01 ------------------------------- */
+
+const names = ["Zsra", "Tariq", "Amir", "Lina"];
+names.reverse();
+console.log(names);
+
+
+// Output: ['Lina', 'Amir', 'Tariq', 'Zsra']
+
+
+//TODO: Reverse the order of numbers in the array
+/* ------------------------------- Problem 02 ------------------------------- */
+
+const numbers = [1, 4, 9, 7];
+numbers.reverse();
+console.log(numbers);
+
+
+// Output: [7, 9, 4, 1]
+
+
+//TODO: Reverse the order of user objects in the array
+/* ------------------------------- Problem 03 ------------------------------- */
+
+const users = [
+    {name: "Ali", age: 30},
+    {name: "Sara", age: 25},
+    {name: "Tariq", age: 35}
+];
+users.reverse();
+console.log(users);
+
+
+// Output: [{name: "Tariq", age: 35}, {name: "Sara", age: 25}, {name: "Ali", age: 30}]
