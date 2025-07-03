d7273fcccae3b62ea417ee9acd232fbd5e312290
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-07-03 23:59:50 +0600

    feat: answer some question about CRUD

---
+++ Chapter-19(JSON-Fetch-Promise)/Practice-080(CRUD-Operations)/01-curd-.js
@@ -0,0 +1,57 @@
+
+//TODO: profile single or married, if this type of data is changed, will it be PUT or PATCH?
+/* ------------------------------- Problem 01 ------------------------------- */
+
+// Answer:
+//* Replace full data           	=> PUT
+//* Change Partial data only	    => PATCH
+
+//? changing profile from "single" to "married" so we need to use   => PATCH
+
+
+//TODO: Which HTTP method is used to add new information to the database?
+/* ------------------------------- Problem 02 ------------------------------- */
+
+//* Answer: POST – request used to insert data. Posted data type – JSON.
+
+
+//TODO: What kind of operation occurs when I go to watch a video on a website?
+/* ------------------------------- Problem 03 ------------------------------- */
+
+/* Answer: 
+*   fetch() is used to communicate with the backend (usually via REST APIs)
+*   Promise handles async tasks like getting video URLs, user info, ads
+*   CRUD actions are triggered in the background:
+*       Read video info
+*       Create/Update viewing progress
+*       Delete history items or logs
+*/
+
+
+//TODO: What is CRUD?
+/* ------------------------------- Problem 04 ------------------------------- */
+
+//? Answer: 
+//* CRUD stands for Create, Read, Update, Delete — the four basic actions you can do with data.
+
+//*    Create → Add new data
+//*    Read → Get or view data
+//*    Update → Change existing data
+//*    Delete → Remove data
+//*    Used in websites, apps, and databases to manage content like videos, users, comments, etc
+
+
+//TODO: What are the methods or varb of HTTP? Which one works?
+/* ------------------------------- Problem 05 ------------------------------- */
+
+
+//? Answer: 
+//*  HTTP Methods (Verbs):
+
+//*    GET – Get data
+//*    POST – Create data
+//*    PUT – Update/replace data
+//*    PATCH – Update part of data
+//*    DELETE – Delete data
+
+//*    Each method works for a specific purpose in web communication.
