28d00ca27eeb106b09ffd35e549688cb698b634c
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-07-14 22:50:04 +0600

    feat: use strict for prevent unusual behaviour :)

---
+++ Chapter-23(JS-Tricky-Concept)/Practice-One-Hundred(Use-Strict)/01-use-strict.js
@@ -0,0 +1,22 @@
+
+//TODO: Why is the strict mood used?
+/* ------------------------------- Problem 01 ------------------------------- */
+
+//? Answer: 
+/*
+    Prevent accidental bugs (like undeclared variables).
+    Make JavaScript more secure and predictable.
+    Catch silent errors by throwing exceptions.
+    Disallow unsafe or confusing syntax (like with, duplicate params).
+    Help modern engines optimize your code better.
+*/
+
+//* Example:
+/*
+    1. var
+    2. eval
+    3. with
+    4. function parameter (x, x)
+    5. var object porperties (obj: 1, obj: 2)
+    ete...
+*/
