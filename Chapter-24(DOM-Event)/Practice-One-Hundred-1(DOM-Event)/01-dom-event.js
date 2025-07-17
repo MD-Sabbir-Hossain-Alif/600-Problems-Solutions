8f6a0197a9661f51fc70d066044337f14e31b0e8
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2025-07-18 00:01:26 +0600

    feat: update

---
+++ Chapter-24(DOM-Event)/Practice-One-Hundred-1(DOM-Event)/01-dom-event.js
@@ -0,0 +1,53 @@
+
+//TODO: What is the difference between local storage and session storage?
+/* ------------------------------- Problem 01 ------------------------------- */
+
+//? Answer: 
+/*
+?       localStorage	                                    sessionStorage
+
+1. Persists even after the browser is closed        1. Only lasts for the duration of the tab/session
+2. Shared across all tabs/windows of same origin    2. Unique to each tab/window
+3. 	~5–10MB (varies by browser)                     3. ~5–10MB (same as localStorage)
+4. Store user settings, tokens, cached data         4. Temporary data like form state, navigation data
+5. User manually clears it or via code              5. Tab is closed or page is reloaded
+6. localStorage.getItem(), setItem()                6. sessionStorage.getItem(), setItem()
+
+*/
+
+
+//TODO: 
+/* ------------------------------- Problem 02 ------------------------------- */
+// saving 
+let mode = "dark";
+
+
+
+// Output: 
+
+
+//TODO: 
+/* ------------------------------- Problem 03 ------------------------------- */
+
+
+
+
+// Output: 
+
+
+//TODO: 
+/* ------------------------------- Problem 04 ------------------------------- */
+
+
+
+
+// Output: 
+
+
+//TODO: 
+/* ------------------------------- Problem 05 ------------------------------- */
+
+
+
+
+// Output: 
